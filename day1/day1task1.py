4a563442a0dedc659b129b928f442a4b2f51ca06
I538826 <shadeiax@gmail.com>
2021-12-02 21:21:19 +0100

    shortened code for day2

--- day1/day1task1.py
+++ day1/day1task1.py
@@ -1,12 +1,8 @@
 f = open("measures", "r")
 lines = f.readlines()
-
 counter = 0
-
 for i in range(len(lines) - 1):
     if lines[i + 1] > lines[i]:
         counter += 1
-
     i += 1
-
 print(counter)
--- day2/day2task2.py
+++ day2/day2task2.py
@@ -5,13 +5,11 @@ hor_pos = 0
 aim = 0
 for line in lines:
     split_line = line.split(" ")
-    operation = split_line[0]
-    amount = split_line[1]
-    if operation == "forward":
-        hor_pos += int(amount)
-        depth += aim * int(amount)
-    elif operation == "down":
-        aim += int(amount)
-    elif operation == "up":
-        aim -= int(amount)
+    if split_line[0] == "forward":
+        hor_pos += int(split_line[1])
+        depth += aim * int(split_line[1])
+    elif split_line[0] == "down":
+        aim += int(split_line[1])
+    elif split_line[0] == "up":
+        aim -= int(split_line[1])
 print(depth * hor_pos)
